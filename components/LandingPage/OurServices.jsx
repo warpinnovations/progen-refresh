5ca24f06ba7c8db7ab0c91aa773d28caf583a8cb
Andry T <andryjosephtumacole@gmail.com>
2025-06-23 20:51:07 +0800

    fix(landing/services): tweaked animation to be smoother

--- components/LandingPage/OurServices.jsx
+++ components/LandingPage/OurServices.jsx
@@ -30,22 +30,27 @@ function OurServices(){
 
     return <div
     className='w-full flex flex-col justify-center md:py-40 mb-8'
-
     >
-       <div
+       <motion.div
         ref={ref}
         className="grid grid-cols-1 md:grid-cols-2 justify-center items-center text-center z-10 mb-20 "
+        initial="hidden"
+        animate={isVisibleInAnimation ? "visible":"hidden"}
+        variants={{
+            hidden:{opacity: 0, y:20},
+            visible: {
+                opacity: 1,
+                y: "0%",
+                transition:{
+                    duration: 0.6,
+                    ease: "easeInOut",
+                    delayChildren: .15,
+                    staggerChildren: 0.15
+                }
+            }
+        }}
         >
-            <motion.div className="w-full flex justify-center md:justify-end lg:justify-center lg:pr-10"
-            initial={{ opacity: 0, scale: 0.0001, y: 300 }}
-            animate={{
-                opacity: isVisibleInAnimation ? 1 : 0,
-                y: isVisibleInAnimation ? 0 : 20,
-                scale: isVisibleInAnimation ? 1 : 0,
-                transition: {duration: 1},
-            }}
-            exit={{ opacity: 0, y: -20 }}
-            >
+            <motion.div className="w-full flex justify-center md:justify-end lg:justify-center lg:pr-10">
                 <div className="flex flex-row md:flex-col mb-5 md:mb-20 gap-2 md:gap-4 w-fit justify-center md:justify-start mt-10">
                     <h1 className={`${MoonlanderFont.className} flex font-black justify-center text-md md:text-5xl lg:w-1/3 text-[#f5f5f5] md:justify-start `}>
                         OUR
@@ -60,19 +65,16 @@ function OurServices(){
                     <motion.div
                         key={index}
                         className={`flex relative md:p-5 justify-center md:justify-start`}
-                        initial="hidden"
-                        whileInView="visible"
-                        viewport={{once: true}}
                         variants={{
                             hidden: {opacity: 0, x: "30%"},
-                            visible: {opacity: 1, x:"0%", transition: {type: "spring"}},
+                            visible: {opacity: 1, x:"0%", transition: {type: "spring", stiffness: 120, damping: 20 }},
                         }}
                     >
                         <p className="text-center md:text-left ">{item}</p>
                     </motion.div>
                 ))}
            </div>
-       </div>
+       </motion.div>
    </div>
 }
 
