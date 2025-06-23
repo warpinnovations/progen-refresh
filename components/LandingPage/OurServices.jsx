e8474ffb3e1bbf10f0a3031e327828f0c3f1f7ac
Andry T <andryjosephtumacole@gmail.com>
2025-06-23 19:53:25 +0800

    feat(landing:services): added animation  on-showup on viewport

--- components/LandingPage/OurServices.jsx
+++ components/LandingPage/OurServices.jsx
@@ -26,6 +26,11 @@ function OurServices(){
         threshold: 0.7,
     });
 
+    const animationSlideFromRight = {
+        hidden: {opacity: 0, x: "50%"},
+        visible: {opacity: 1, x:"0%", transition: {type: "spring"}},
+    };
+
     const isVisibleInAnimation = inView;
 
     return <div
@@ -33,22 +38,19 @@ function OurServices(){
 
     >
        <motion.div
-            ref={ref}
-           className="grid grid-cols-1 md:grid-cols-2 justify-center items-center text-center z-10 mb-20 "
-           initial={{ opacity: 0, scale: 0.0001, y: 300 }}
-           animate={{
-               opacity: isVisibleInAnimation ? 1 : 0,
-               y: isVisibleInAnimation ? 0 : 20,
-               scale: isVisibleInAnimation ? 1 : 0,
-               transition: {
-                   duration: 1,
-                
-
-               },
-           }}
-           exit={{ opacity: 0, y: -20 }}
-           >
-            <div className="w-full flex justify-center md:justify-end lg:justify-center lg:pr-10">
+        ref={ref}
+        className="grid grid-cols-1 md:grid-cols-2 justify-center items-center text-center z-10 mb-20 "
+        >
+            <motion.div className="w-full flex justify-center md:justify-end lg:justify-center lg:pr-10"
+            initial={{ opacity: 0, scale: 0.0001, y: 300 }}
+            animate={{
+                opacity: isVisibleInAnimation ? 1 : 0,
+                y: isVisibleInAnimation ? 0 : 20,
+                scale: isVisibleInAnimation ? 1 : 0,
+                transition: {duration: 1},
+            }}
+            exit={{ opacity: 0, y: -20 }}
+            >
                 <div className="flex flex-row md:flex-col mb-5 md:mb-20 gap-2 md:gap-4 w-fit justify-center md:justify-start mt-10">
                     <h1 className={`${MoonlanderFont.className} flex font-black justify-center text-md md:text-5xl lg:w-1/3 text-[#f5f5f5] md:justify-start `}>
                         OUR
@@ -57,26 +59,24 @@ function OurServices(){
                         SERVICES
                     </h1>
                 </div>
-            </div>
+            </motion.div>
            <div className={`${MoonlanderFont.className} text-md md:text-4xl text-white flex flex-col gap-2 `}>
                 {services.map((item, index) => (
                     <motion.div
                         key={index}
                         className={`flex relative md:p-5 justify-center md:justify-start`}
-                        initial={{ opacity: 0, y: 20 }}
-                        animate={{
-                            opacity: inView ? 1 : 0,
-                            y: inView ? 0 : 20,
+                        initial="hidden"
+                        whileInView="visible"
+                        viewport={{once: true}}
+                        variants={{
+                            hidden: {opacity: 0, x: "30%"},
+                            visible: {opacity: 1, x:"0%", transition: {type: "spring"}},
                         }}
                     >
-                        <p className="text-center md:text-left ">
-                        {item}
-
-                        </p>
+                        <p className="text-center md:text-left ">{item}</p>
                     </motion.div>
                 ))}
            </div>
-
        </motion.div>
    </div>
 }
