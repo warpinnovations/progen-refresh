58415304a071230f721d01429b1d43fb66f7a2ec
Andry Tumacole <ajtajaja13@gmail.com>
2025-07-17 15:49:48 +0800

    feat(landing/certifications): setup for animation

--- components/LandingPage/Certifications.jsx
+++ components/LandingPage/Certifications.jsx
@@ -1,8 +1,17 @@
 /* eslint-disable @next/next/no-img-element */
+"use client"
+import { motion } from 'framer-motion';
+import { useInView } from 'react-intersection-observer';
 import localFont from 'next/font/local';
 const MoonlanderFont = localFont({src:'../../Fonts/Moonlander.ttf'});
 
 function Certifications(){
+    const [ref, inView] = useInView({
+        triggerOnce: true,
+        threshold: 0.7,
+    });
+
+    const isVisibleInAnimation = inView;
 
     const certificates = [
         {
@@ -37,34 +46,36 @@ function Certifications(){
     return <section
     className='w-full flex flex-col justify-center md:py-40 mb-8'
     >
-        <div className="container mx-auto px-4 text-center text-white">
+        <motion.div className="container mx-auto px-4 text-center text-white">
             <h1 className={`${MoonlanderFont.className} font-black justify-center text-md md:text-4xl text-prOrange mb-32 text-center`}>
                 CERTIFICATIONS
             </h1>
             {/* 3-column rows */}
             <div className="grid grid-cols-1 md:grid-cols-3 gap-y-12 md:gap-x-10">
                 {certificates.slice(0, lenTopRows).map((cert, i) => (
-                    <div key={cert.emblems + cert.titles + i.toString()} className="flex flex-col items-center">
-                    <div className="flex space-x-2 mb-4">
-                    {cert.emblems.map((src, i) => (
-                        <img key={src + i.toString()} src={src} alt="" className="h-32 w-auto" />
-                    ))}
-                    </div>
-            
-                    <p className="text-sm uppercase tracking-wider">{cert.linkPhrase}</p>
-            
-                    {cert.titles.map((line, i) => (
-                    <p key={JSON.stringify(cert.titles) + line} className="mt-1 font-bold uppercase text-center">
-                        {line}
-                    </p>
-                    ))}
-                </div>
+                    <motion.div key={cert.emblems + cert.titles + i.toString()} className="flex flex-col items-center">
+                        <div className="flex space-x-2 mb-4">
+                            {cert.emblems.map((src, i) => (
+                                <img key={src + i.toString()} src={src} alt="" className="h-32 w-auto" />
+                            ))}
+                        </div>
+                
+                        <p className="text-sm uppercase tracking-wider">{cert.linkPhrase}</p>
+                
+                        {cert.titles.map((line, i) => (
+                            <p key={JSON.stringify(cert.titles) + line} className="mt-1 font-bold uppercase text-center">
+                                {line}
+                            </p>
+                        ))}
+                    </motion.div>
                 ))}
             </div>
             {/* centered bottom rows regardless of item nums*/}
             <div className="flex flex-col md:flex-row items-center justify-center gap-x-52 gap-y-12 mt-16">
                 {certificates.slice(lenTopRows).map((cert, i) => (
-                    <div key={JSON.stringify(cert.titles) + i.toString()} className="flex flex-col items-center">
+                    <motion.div key={JSON.stringify(cert.titles) + i.toString()} className="flex flex-col items-center"
+                    // variants={}
+                    >
                         <div className="flex space-x-2 mb-4">
                             {cert.emblems.map((src, i) => (
                                 <img key={JSON.stringify(cert.emblems) + i.toString()} src={src} alt="" className="h-32 w-auto" />
@@ -76,10 +87,10 @@ function Certifications(){
                                     {line}
                                 </p>
                             ))}
-                    </div>
+                    </motion.div>
                 ))}
             </div>
-        </div>
+        </motion.div>
     </section>
 }
 
