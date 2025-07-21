35c0cde8938eaca608822cd9e565bc46c983d75a
Badaboopie <domdicar@gmail.com>
2025-07-21 18:04:11 +0800

    Fix: Polished Mobile view and Desktop view of Awards Section

--- components/LandingPage/Awards.jsx
+++ components/LandingPage/Awards.jsx
@@ -40,9 +40,9 @@ const slideVariants = {
     }),
     center: {
         zIndex: 1,
-        x: 100,
+        x: 0,
         opacity: 1,
-        scale: 1.5,
+        scale: 1,
     },
     exit: (direction) => ({
         zIndex: 0,
@@ -92,20 +92,20 @@ const Awards = () => {
         };
 
     return (
-        <main className="min-h-screen bg-cover bg-center bg-no-repeat bg-[url('/LandingPageAssets/awardsbg.png')]">
+        <main className="min-h-120vh md:min-h-90vh bg-cover bg-center bg-no-repeat bg-[url('/LandingPageAssets/awardsbg.png')] py-40">
             <div className='z-50'>
-                <h1 className={`${MoonlanderFont.className} text-center font-black justify-center text-md md:text-4xl text-prOrange mb-20`}>AWARDS</h1>
-                <div className='relative w-full h-[500px] flex items-center justify-center overflow-hidden '>
+                <h1 className={`${MoonlanderFont.className} text-center font-black justify-center text-md md:text-4xl text-prOrange md:mb-20`}>AWARDS</h1>
+                <div className='relative w-full h-[500px] md:h-[700px] flex items-center justify-center overflow-hidden md:pb-80'>
                     <button
                         onClick={() => paginate(-1)}
-                        className="absolute left-4 top-1/2 -translate-y-1/2 z-20 flex h-full w-12 items-center justify-center rounded-full text-opacity-50 text-white text-2xl backdrop-blur-sm transition-all hover:bg-gradient-to-bl from-white/0 to-white/10 "
+                        className="absolute left-4 top-1/2 -translate-y-1/2 z-20 flex h-full w-12 items-center justify-center rounded-full text-opacity-10 hover:text-opacity-100 text-white text-2xl transition-all hover:bg-gradient-to-bl from-white/0 to-white/10 "
                         aria-label="Previous slide"
                     >
                         ❮
                     </button>
                     <button
                         onClick={() => paginate(1)}
-                        className="absolute right-4 top-1/2 -translate-y-1/2 z-20 flex h-full w-12 items-center justify-center rounded-full text-opacity-50 text-white text-2xl backdrop-blur-sm transition-all hover:bg-gradient-to-br from-white/0 to-white/10"
+                        className="absolute right-4 top-1/2 -translate-y-1/2 z-20 flex h-full w-12 items-center justify-center rounded-full text-opacity-10 hover:text-opacity-100 text-white text-2xl transition-all hover:bg-gradient-to-br from-white/0 to-white/10"
                         aria-label="Next slide"
                     >
                         ❯
@@ -121,44 +121,101 @@ const Awards = () => {
                             animate="center"
                             exit="exit"
                             transition={{
-                                duration: 1, ease: "easeInOut", delay: 0.5
+                                duration: 1, ease: "easeInOut", delay: 0.05
                             }}
+                            className="absolute w-full h-full flex items-center justify-center p-4"
                         >
-                            <div className='relative flex flex-col z-10 md:flex-row items-center pl-[130px]'>
+                            <div className='relative flex flex-col items-center z-10 md:flex-row md:items-center md:justify-start md:pl-[130px]'>
+                                <div className='flex items-center justify-center space-x-4 p-4 md:hidden'>
+
+                            {/* MobileView */}
+                                    <motion.div
+                                        key={awarditems[currentIndex].awardimg}
+                                        initial={{ opacity: 0, clipPath: 'inset(0% 0% 100% 0%)' }}
+                                        animate={{ opacity: 1, clipPath: 'inset(0% 0% 0% 0%)'}}
+                                        transition={{
+                                            duration: 1.2,
+                                            ease: 'easeInOut',
+                                            delay: 1.5,
+                                        }}
+                                        className='md:relative'
+                                    >
+                                        <Image
+                                            src={awarditems[currentIndex].awardimg}
+                                            alt={awarditems[currentIndex].title}
+                                            height={80}
+                                            width={80}
+                                            className='drop-shadow-lg flex-row'
+                                        />
+                                    </motion.div>
+
+                                    <div className='md:hidden flex flex-col text-white text-center md:text-left md:w-80 m-0'>
+                                        <motion.h3
+                                            key={awarditems[currentIndex].title}
+                                            className='text-md md:text-4xl font-thin tracking-widest uppercase mb:mb-4 mb:h-16 flex items-center justify-start'
+                                            variants={containerVariants}
+                                            initial="hidden"
+                                            animate="visible"
+                                        >
+                                            {awarditems[currentIndex].title.split('').map((char, index) => (
+                                                <motion.span
+                                                    key={index}
+                                                    variants={characterVariants}
+                                                >
+                                                    {char === ' ' ? '\u00A0' : char}
+                                                </motion.span>
+                                            ))}
+                                        </motion.h3>
+                                        <motion.h4
+                                            key={awarditems[currentIndex].category}
+                                            initial={{ opacity: 0, y: 20 * direction}}
+                                            animate={{ opacity: 1, y: 0}}
+                                            exit={{ opacity: 0, y: 0}}
+                                            transition={{
+                                                duration: 0.8,
+                                                ease: 'easeInOut',
+                                                delay: 1.5,
+                                            }}
+                                            className='text-md md:text-3xl text-[#DAAF5B] font-semibold text-start'
+                                        >
+                                            {awarditems[currentIndex].category}
+                                        </motion.h4>
+                                    </div>
+                                </div>
+
+                                {/* Desktop View */}
                                 <motion.div
-                                    key={awarditems[currentIndex].awardimg}
-                                    initial={{ opacity: 0, y: 20 * direction}}
-                                    animate={{ opacity: 1, y: 0}}
-                                    transition={{
-                                        duration: 0.9,
-                                        ease: 'easeInOut',
-                                        delay: 1,
-                                    }}
-                                >
-                                    <Image
-                                        src={awarditems[currentIndex].awardimg}
-                                        alt={awarditems[currentIndex].title}
-                                        height={100}
-                                        width={100}
-                                        className='absolute drop-shadow-lg top-[70px] left-0'
-                                    />
+                                        key={awarditems[currentIndex].awardimg}
+                                        initial={{ opacity: 0, clipPath: 'inset(0% 0% 100% 0%)' }}
+                                        animate={{ opacity: 1, clipPath: 'inset(0% 0% 0% 0%)'}}
+                                        transition={{
+                                            duration: 1.2,
+                                            ease: 'easeInOut',
+                                            delay: 1.5,
+                                        }}
+                                        className='md:relative md:ml-20'
+                                    >
+                                        <Image
+                                            src={awarditems[currentIndex].awardimg}
+                                            alt={awarditems[currentIndex].title}
+                                            height={170}
+                                            width={170}
+                                            className='hidden md:block md:relative'
+                                        />
                                 </motion.div>
-                                <motion.div
-                                    className="absolute left-[91px] top-[120px] z-10 w-[130px] h-[2px] bg-[#DAAF5B] rounded-full rotate-45 origin-left"
-                                    initial={{ scaleX: 0 }}
-                                    animate={{ scaleX: 1 }}
-                                    transition={{ duration: 0.6, ease: "easeInOut", delay: 2.5 }}
-                                    />
-                                
-                                <Image
-                                    src={awarditems[currentIndex].planetimg}
-                                    alt={awarditems[currentIndex].title}
-                                    height={300}
-                                    width={300}
-                                    className='drop-shadow-lg p-0 m-0'
-                                />    
-                        
-                                <div className='text-white text-center md:text-left w-full md:w-80'>
+
+                                <div className='inline-block my-4 size-80 md:size-[500px]'>
+                                    <Image  
+                                        src={awarditems[currentIndex].planetimg}
+                                        alt={awarditems[currentIndex].title}
+                                        width={0}
+                                        height={0}
+                                        sizes='200vw'
+                                        className='drop-shadow-lg size-full object-contain'
+                                    />    
+                                </div>
+
+                                <div className='hidden md:flex md:flex-col text-white text-center md:text-left w-full md:w-80'>
                                     <motion.h3
                                         key={awarditems[currentIndex].title}
                                         className='text-4xl font-thin tracking-widest uppercase mb-4 h-16 flex items-center justify-center md:justify-start'
