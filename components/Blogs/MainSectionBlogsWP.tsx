ae032fcbc08d04f33e2f10377f93c9a1caa32850
Dominic Bernas <nickobernas795@gmail.com>
2025-11-10 14:06:21 +0800

    fix: blog-footer-duplication

--- components/Blogs/MainSectionBlogsWP.tsx
+++ components/Blogs/MainSectionBlogsWP.tsx
@@ -126,7 +126,7 @@ const MainSectionBlogsWP = (props : {data: WPPost[]}) => {
         )}
 
         <ThreeColumnFooter />
-        <Footer />
+        {/* <Footer /> */}
       </PageTransition>
     </div>
   );
