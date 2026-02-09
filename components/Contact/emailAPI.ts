25f19f2342b5b4c5479b7bf0488bdf27c93c4750
Dominic Bernas <nickobernas795@gmail.com>
2026-02-09 15:36:36 +0800

    fix: emailAPI.ts-id-issues

--- components/Contact/emailAPI.ts
+++ components/Contact/emailAPI.ts
@@ -1,12 +1,12 @@
 import emailjs from '@emailjs/browser';
 
-type emailProps ={
-  to_name:string,
-  from_email:string
-  user_email:string,
-  number:string,
-  from_name?:string
-  message:string
+type emailProps = {
+  to_name: string,
+  from_email: string
+  user_email: string,
+  number: string,
+  from_name?: string
+  message: string
 }
 
 const sendEmail = ({
@@ -16,22 +16,27 @@ const sendEmail = ({
   user_email,
   message,
   number
-}:emailProps) => {
-  emailjs.send( process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID!,
-    process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID!, {
-    from_email,
-    from_name,
-    to_name,
-    user_email,
-    message,
-    number
+}: emailProps) => {
+  return emailjs.send(
+    'service_gv172m7',
+    'template_1nba5fj',
+    {
+      from_email,
+      from_name,
+      to_name,
+      user_email,
+      message,
+      number
     },
-    process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_KEY as string,               
-  ) 
-    .then(function(response) {
-    console.log('SUCCESS!', response.status, response.text);
-    }, function(error) {
-    console.log('FAILED...', error);
+    'aYz5scxr20cYdrfHZ',
+  )
+    .then(function (response) {
+      console.log('SUCCESS!', response.status, response.text);
+      return response;
+    })
+    .catch(function (error) {
+      console.log('FAILED...', error);
+      throw error;
     });
 }
 
